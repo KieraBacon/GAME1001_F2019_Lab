3d7394fa05912ae1be8d741f4e13a4db1c174ce3
Kiera Bacon <101050135@georgebrown.ca>
2019-10-03 13:16:20 -0400

    [ADD] Example2

---
+++ Week5/Example2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num = 4;
+
+	//Prefix & postfix increment
+
+	cout << num		<< "   ";
+	cout << num++	<< "   ";
+	cout << num		<< "   ";
+	cout << ++num	<< "   ";
+
+	system("pause");
+	return 0;
+}
