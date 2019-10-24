8cef18371694972a3d25632deacfc13436406353
Kiera Bacon <kierajbacon@outlook.com>
2019-10-23 22:50:57 -0400

    ADD: _getch() example

---
+++ Week 8/getchTest.cpp
@@ -0,0 +1,16 @@
+//#include <conio.h>
+//#include <stdio.h>
+//#include <stdlib.h>
+//using namespace std;
+//
+//int main()
+//{
+//	system("COLOR F0");
+//	char ch;
+//	do
+//	{
+//		ch = _getch();
+//		printf("%c", ch);
+//	} while (ch != 27);
+//	return 0;
+//}
